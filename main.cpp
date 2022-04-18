96d42a7b6027cfe625921857a4084b5959109d0d
joye2003 <leoscchou@gmail.com>
2022-04-18 22:24:39 +0800

    first

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by 周儀 on 2022/4/18.
+//
+
+#include "main.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by 周儀 on 2022/4/18.
+//
+
+#ifndef PRACTICE5_MAIN_H
+#define PRACTICE5_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //PRACTICE5_MAIN_H
